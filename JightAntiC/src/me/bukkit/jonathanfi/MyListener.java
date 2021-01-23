21dbb95f4f6b9287a1e4ca0f5c7580b50125dc0a
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2021-01-24 00:08:32 +0100

    Update MyListener.java

--- JightAntiC/src/me/bukkit/jonathanfi/MyListener.java
+++ JightAntiC/src/me/bukkit/jonathanfi/MyListener.java
@@ -67,9 +67,9 @@ public class MyListener extends Thread implements Listener {
 						for(int i=-offset;i<offset;i++) {
 							if(Lloc.getWorld().getBlockAt(Lloc.getBlockX()-i,Lloc.getBlockY()-1,loc.getBlockZ()-i).getType() != Material.AIR) {blockNear = true;}
 						}
-						 if(loc.getWorld().getBlockAt(loc.getBlockX(),(int)(loc.getBlockY()-loc.distance(Lloc)),loc.getBlockZ()).getType() == Material.AIR && Lloc.getWorld().getBlockAt(Lloc.getBlockX(),Lloc.getBlockY()-1,Lloc.getBlockZ()).getType() == Material.AIR && !blockNear) {event.setCancelled(true);if(JLA.log)System.out.println(player+" AntiFlight1");}else{cancel = false;int airs = 0;for(int i=0; i < 2; i++){if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY()-i,loc.getBlockZ()).getType() != Material.AIR) {cancel=false;}else{airs = airs + 1;}}if(airs>3) {JLA.action("\nPlease stop!",event.getPlayer());}else if(airs > 2) {cancel=true;}if(cancel&&mayCheat>1) {event.setCancelled(cancel);if(JLA.log)System.out.println(player+" AntiFlight2");}}
+						 if(loc.getWorld().getBlockAt(loc.getBlockX(),(int)(loc.getBlockY()-loc.distance(Lloc)),loc.getBlockZ()).getType() == Material.AIR && Lloc.getWorld().getBlockAt(Lloc.getBlockX(),Lloc.getBlockY()-1,Lloc.getBlockZ()).getType() == Material.AIR && !blockNear) {event.setCancelled(true);if(JLA.log)System.out.println(player+" AntiFlight1");JLA.action("Stop flycheating!", event.getPlayer());}else{cancel = false;int airs = 0;for(int i=0; i < 2; i++){if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY()-i,loc.getBlockZ()).getType() != Material.AIR) {cancel=false;}else{airs = airs + 1;}}if(airs>3) {JLA.action("\nPlease stop!",event.getPlayer());}else if(airs > 2) {cancel=true;}if(cancel&&mayCheat>1) {event.setCancelled(cancel);if(JLA.log)System.out.println(player+" AntiFlight2");}}
 					}
-					try {if(event.getPlayer().getPassengers() != null) {if(JLA.log)System.out.println(event.getPlayer().getPassengers());}}catch(NoSuchMethodError e) {}
+					try {if(event.getPlayer().getPassengers() != null && event.getPlayer().getPassengers().size()>0) {if(JLA.log)System.out.println(event.getPlayer().getPassengers());}}catch(NoSuchMethodError e) {}
 				 }
 			}
 			if(JLA.atp) {cancel = false;
@@ -79,11 +79,17 @@ public class MyListener extends Thread implements Listener {
 					//Location loca = new Location(Lloc.getWorld(), Lloc.getBlockX(), Lloc.getBlockY(), Lloc.getBlockZ());
 					//event.getPlayer().teleport(loca);
 				}
-				if(cancel) {loc = Lloc;event.setCancelled(cancel);if(loc.distance(Lloc)>5) {event.getPlayer().teleport(Lloc);PlayerData.remove(player);}JLA.action("\n§6Please stop!", event.getPlayer());if(JLA.log)System.out.println("AntiTP for: "+player);}
+				if(cancel) {loc = Lloc;if(loc.distance(Lloc)>5) {event.getPlayer().teleport(Lloc);PlayerData.remove(player);}else{event.setCancelled(cancel);}JLA.action("\n§6Please stop!", event.getPlayer());PlayerData.remove(player);if(JLA.log)System.out.println("AntiTP for: "+player);}
 			}
 			if(JLA.aliq>0) {
-				if(PlayerDPS.get(player).get(3) == null) {PlayerDPS.get(player).set(3, (float)Dnow.getTime());}
-				if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY(),loc.getBlockZ()).getType() == Material.WATER) {PlayerDPS.get(player).set(3, (float)Dnow.getTime());}if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY()-1,loc.getBlockZ()).getType() == Material.WATER && (float)Dnow.getTime() - (float)PlayerDPS.get(player).get(3)>2000) {event.getPlayer().teleport(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY()-JLA.aliq, loc.getBlockZ()));}
+				if(PlayerDPS.get(player)!=null) {if(PlayerDPS.get(player).get(3) != null) {}else{PlayerDPS.get(player).set(3, (float)Dnow.getTime());}}else{List<Object>tmp = new ArrayList<Object>();
+				tmp.add((float)loc.distance(Lloc));
+				tmp.add((float)Dnow.getTime());
+				tmp.add((float)Dnow.getTime());
+				tmp.add((float)Dnow.getTime());
+				PlayerDPS.put(player, tmp);}
+				//System.out.println(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY(),loc.getBlockZ()).getType()+":"+loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY()-1,loc.getBlockZ()).getType());
+				if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY(),loc.getBlockZ()).getType() == Material.WATER) {PlayerDPS.get(player).set(3, (float)Dnow.getTime());}else if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY()-1,loc.getBlockZ()).getType() == Material.WATER && (float)Dnow.getTime() - (float)PlayerDPS.get(player).get(3)>2000) {event.getPlayer().teleport(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY()-JLA.aliq, loc.getBlockZ()));PlayerDPS.get(player).set(3, Dnow.getTime());}
 			}
 			if(cancel != true && JLA.bmps>0) {
 				if(PlayerDPS.get(player) != null) {
@@ -113,23 +119,16 @@ public class MyListener extends Thread implements Listener {
 	}
 	public void onTeleport(PlayerTeleportEvent e) {
 		final String player = e.getPlayer().getName();
-		if(PlayerData.get(player) != null) {
-			PlayerData.remove(player);
-		}
+		PlayerData.remove(player);
 	}
 	public void onDeath(PlayerDeathEvent e) {
-		final String player = e.getEntity().getName();
-		if(PlayerData.get(player) != null) {
-			PlayerData.remove(player);
-		}
+		final String player = e.getEntity().getName();PlayerData.remove(player);PCD.remove(player);PlastB.remove(player);PlayerDPS.remove(player);
 	}
 	public static Map<String, Long> PCD = new HashMap<String, Long>();
 	@EventHandler (priority = EventPriority.LOWEST)
 	public void onPlayerDmg(EntityDamageByEntityEvent event) {
 		if(event.getEntity().getType() == EntityType.PLAYER) {if(event.getEntity().isDead()){final String player = event.getEntity().getName();
-		if(PlayerData.get(player) != null) {
-			PlayerData.remove(player);
-		}PCD.remove(player);}}
+			PlayerData.remove(player);PCD.remove(player);PlastB.remove(player);PlayerDPS.remove(player);}}
 		if(event.getDamager().getType() == EntityType.PLAYER) {
 			if(!event.getDamager().hasPermission("jla.cert")) {
 			if(JLA.ar>0) {
@@ -140,7 +139,7 @@ public class MyListener extends Thread implements Listener {
 			}
 			if(JLA.aka>0) {//thanks https://bukkit.org/threads/get-if-player-is-looking-at-an-entity.106661/!
 				Player player = (Player) event.getDamager();
-				if(JLA.aka==1) {if(player.getEyeLocation() != event.getEntity().getLocation()) {event.setCancelled(true);JLA.action("\nPlease stop!",player);}}else if(JLA.aka==2) {
+				if(JLA.aka==1) {if(player.getEyeLocation() != event.getEntity().getLocation()) {event.setCancelled(true);JLA.action("\nPlease stop with Aura!",player);}}else if(JLA.aka==2) {
 					
 				}
 			}
@@ -162,7 +161,7 @@ public class MyListener extends Thread implements Listener {
 		}
 		if(PlayerDPS.get(player) != null) {
 			PlayerDPS.remove(player);
-		}
+		}PDD.remove(player);
 		if(PlastB.containsKey(player))PlastB.remove(player);
 		if(PCD.get(player) != null && JLA.acl != 0) {
 			Calendar c1 = Calendar.getInstance();
