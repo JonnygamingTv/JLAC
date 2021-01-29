550674a1655339caab4d0d1e12487948a6798c88
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2021-01-29 15:21:42 +0100

    Update MyListener.java

--- JightAntiC/src/me/bukkit/jonathanfi/MyListener.java
+++ JightAntiC/src/me/bukkit/jonathanfi/MyListener.java
@@ -60,7 +60,7 @@ public class MyListener extends Thread implements Listener {
 					}}else if(PlastB.containsKey(player)){PlastB.remove(player);}
 				 if(loc.distance(Lloc)>0.1) {
 					if(loc.getBlockY()>Lloc.getBlockY()+0.3) if(event.getPlayer().getInventory().getChestplate() != null && event.getPlayer().getInventory().getChestplate().getType() != Material.ELYTRA || (!event.getPlayer().hasPotionEffect(PotionEffectType.JUMP) || !event.getPlayer().hasPotionEffect(PotionEffectType.LEVITATION))) {
-						boolean blockNear = false;int offset=2+JLA.aflight;
+						boolean blockNear = false;int offset=1+JLA.aflight;
 						try {
 						org.bukkit.potion.PotionEffect speed = event.getPlayer().getPotionEffect(PotionEffectType.SPEED);
 						if(speed!=null)if(speed.getAmplifier()>0)offset+=Math.round(speed.getAmplifier()/1000);
@@ -68,7 +68,7 @@ public class MyListener extends Thread implements Listener {
 						for(int i=-offset;i<offset;i++) {
 							if(Lloc.getWorld().getBlockAt(Lloc.getBlockX()-i,Lloc.getBlockY()-1,loc.getBlockZ()-i).getType() != Material.AIR || Lloc.getWorld().getBlockAt(Lloc.getBlockX()-i,Lloc.getBlockY()-2,loc.getBlockZ()-i).getType() != Material.AIR) {blockNear = true;}
 						}
-						 if(loc.getWorld().getBlockAt(loc.getBlockX(),(int)(loc.getBlockY()-loc.distance(Lloc)),loc.getBlockZ()).getType() == Material.AIR && Lloc.getWorld().getBlockAt(Lloc.getBlockX(),Lloc.getBlockY()-1,Lloc.getBlockZ()).getType() == Material.AIR && !blockNear) {event.setCancelled(true);if(JLA.log)System.out.println(player+" AntiFlight1");JLA.action("Stop flycheating!", event.getPlayer());}else{cancel = false;int airs = 0;for(int i=0; i < 2; i++){if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY()-i,loc.getBlockZ()).getType() != Material.AIR) {cancel=false;}else{airs = airs + 1;}}if(airs>3) {JLA.action("\nPlease stop!",event.getPlayer());}else if(airs > 2) {cancel=true;}if(cancel&&mayCheat>1) {event.setCancelled(cancel);if(JLA.log)System.out.println(player+" AntiFlight2");}}
+						 if(loc.getWorld().getBlockAt(loc.getBlockX(),(int)(loc.getBlockY()-loc.distance(Lloc)),loc.getBlockZ()).getType() == Material.AIR && Lloc.getWorld().getBlockAt(Lloc.getBlockX(),Lloc.getBlockY()-1,Lloc.getBlockZ()).getType() == Material.AIR && !blockNear) {event.setCancelled(true);if(JLA.log)System.out.println(player+" AntiFlight1");JLA.action("Stop flycheating!", event.getPlayer());}else{cancel = false;int airs = 0;for(int i=0; i < 2; i++){if(loc.getWorld().getBlockAt(loc.getBlockX(),loc.getBlockY()-i,loc.getBlockZ()).getType() != Material.AIR) {cancel=false;}else{airs = airs + 1;}}if(airs>3) {JLA.action("\nPlease stop being in air!",event.getPlayer());}else if(airs > 2) {cancel=true;}if(cancel&&mayCheat>1) {event.setCancelled(cancel);if(JLA.log)System.out.println(player+" AntiFlight2");}}
 					}
 					try {if(event.getPlayer().getPassengers() != null && event.getPlayer().getPassengers().size()>0) {if(JLA.log)System.out.println(event.getPlayer().getPassengers());}}catch(NoSuchMethodError e) {}
 				 }
@@ -80,7 +80,7 @@ public class MyListener extends Thread implements Listener {
 					//Location loca = new Location(Lloc.getWorld(), Lloc.getBlockX(), Lloc.getBlockY(), Lloc.getBlockZ());
 					//event.getPlayer().teleport(loca);
 				}}catch(NoSuchFieldError e) {}
-				if(cancel) {loc = Lloc;if(loc.distance(Lloc)>5) {event.getPlayer().teleport(Lloc);PlayerData.remove(player);}else{event.setCancelled(cancel);}JLA.action("\n§6Please stop!", event.getPlayer());PlayerData.remove(player);if(JLA.log)System.out.println("AntiTP for: "+player);}
+				if(cancel) {loc = Lloc;if(loc.distance(Lloc)>5) {event.getPlayer().teleport(Lloc);PlayerData.remove(player);}else{event.setCancelled(cancel);}JLA.action("\n§6Please stop TP!", event.getPlayer());PlayerData.remove(player);if(JLA.log)System.out.println("AntiTP for: "+player);}
 			}
 			if(JLA.aliq>0) {
 				if(PlayerDPS.get(player)!=null) {if(PlayerDPS.get(player).get(3) != null) {}else{PlayerDPS.get(player).set(3, (float)Dnow.getTime());}}else{List<Object>tmp = new ArrayList<Object>();
