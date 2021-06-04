03d663698cbc159fff5519c95efc5ab9c6855ae7
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2021-06-04 17:29:53 +0200

    Add files via upload

--- JezJoin/src/main/java/me/jonathanfi/ezjoin/JezJoin/Salias.java
+++ JezJoin/src/main/java/me/jonathanfi/ezjoin/JezJoin/Salias.java
@@ -1,8 +1,12 @@
 package me.jonathanfi.ezjoin.JezJoin;
 
+import java.net.InetSocketAddress;
+
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -14,7 +18,14 @@ public class Salias extends Command {
 		if ((sender instanceof ProxiedPlayer)) {
 			sender.sendMessage(new ComponentBuilder("This command is for console.").color(ChatColor.GOLD).create());
 		}else{
-			if(args.length==2) {
+			if(args.length==3) {
+				System.out.println("[JezJoin] Server name: "+args[0]+"\nServer IP: "+args[1]+"\nServer port: "+args[2]);
+				try {
+				InetSocketAddress socketAddress = new InetSocketAddress(args[1], Integer.parseInt(args[2]));
+				ServerInfo info = ProxyServer.getInstance().constructServerInfo(args[0], socketAddress, "JonHosting.com", false);
+				ProxyServer.getInstance().getServers().put(args[0], info);
+				}catch(Exception er) {er.printStackTrace();}
+			}else if(args.length==2) {
 				Db.alias.put(args[0], args[1]);
 				System.out.println("[JezJoin] Set "+args[0]+" to send to "+args[1]+"!");
 			}else {
